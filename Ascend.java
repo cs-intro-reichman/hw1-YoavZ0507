e2b11d450fc9057004a4234d2007fd27ddd7448b
Yoavz0507 <yoav.ziberski@gmail.com>
2024-11-08 18:15:59 +0200

    Task 4

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,28 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+	int input= Integer.parseInt(args[0]);
+	int a= (int)(Math.random()*input)+1;
+	int b= (int)(Math.random()*input)+1;
+	int c= (int)(Math.random()*input)+1;
+
+	int max1= Math.max(a, b);
+	int max2= Math.max(max1,c);  // the max number ans 1
+
+	int min1= Math.min(a, b);
+	int min2=Math.min(min1, c); // the min number ans 2
+
+	int mid= (a+b+c)- (max2+min2);
+
+	System.out.println(a+","+b+","+c);
+
+	System.out.println(min2+","+mid+","+max2);
+
+
+	
+	
+
+
 	}
+
 }
